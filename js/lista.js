21cfe8da78a5c4f65f61bb8f3413fd54e62b3c85
RTBER <diegomorachimo8@gmail.com>
2025-03-03 11:34:16 +0100

    changed

---
+++ js/lista.js
@@ -0,0 +1,16 @@
+
+document.querySelectorAll('.chapter-title').forEach(button => {
+  button.addEventListener('click', () => {
+    const content = button.nextElementSibling;
+    const isExpanded = button.getAttribute('aria-expanded') === 'true';
+
+    // Cambiar la visibilidad del contenido
+    content.classList.toggle('active', !isExpanded);
+    button.setAttribute('aria-expanded', !isExpanded);
+    content.setAttribute('aria-hidden', isExpanded);
+  });
+});
+
+
+
+
